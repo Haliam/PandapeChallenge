8d5fd8b7c7debdfbc53bf01d0ebf4f9c000db88a
Haliam <haliampf@gmail.com>
2023-03-21 21:00:02 +0000

    Add entities Candidate and CandidateExperience

--- src/Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pandape.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Domain/Entities/Candidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pandape.Domain.Entities
+{
+    public class Candidate
+    {
+        public int IdCandidate { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime InsertDate { get; set; }
+
+        public DateTime ModifyDate { get; set; }
+
+    }
+}
