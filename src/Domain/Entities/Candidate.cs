bc8a57bdba81e477f0f0074741d32a19ddf41847
Haliam <haliampf@gmail.com>
2023-03-21 21:48:03 +0000

    Add DataAnnotations to entities

--- src/Domain/Entities/Candidate.cs
+++ src/Domain/Entities/Candidate.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pandape.Domain.Entities
 {
     public class Candidate
     {
+        [Key]
         public int IdCandidate { get; set; }
 
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(150)]
         public string SurName { get; set; }
 
         public DateTime BirthDate { get; set; }
 
+        [MaxLength(250)]
         public string Email { get; set; }
 
         public DateTime InsertDate { get; set; }
 
-        public DateTime ModifyDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
 
     }
 }
--- src/Domain/Entities/CandidateExperience.cs
+++ src/Domain/Entities/CandidateExperience.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pandape.Domain.Entities
 {
     public class CandidateExperience
     {
+        [Key]
         public int IdCandidateExperience { get; set; }
 
+        [ForeignKey("Candidate")]
         public int IdCandidate { get; set; }
 
+        [MaxLength(100)]
         public string Company { get; set; }
 
+        [MaxLength(100)]
         public string Job { get; set; }
 
+        [MaxLength(4000)]
         public string Description { get; set; }
 
         public decimal Salary { get; set; }
 
         public DateTime BeginDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
         public DateTime InsertDate { get; set; }
 
-        public DateTime ModifyDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
     }
 }
