f3f7e1939dbd86c7bca30af573db7bf1f2aa858f
Haliam <haliampf@gmail.com>
2023-03-22 15:00:09 +0000

    Add CandidatesController, Context and Views with Saffolding and Entity FrameworkCore 3.1.32

---
+++ src/Host/Pandape.Host.Mvc/Data/PandapeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pandape.Domain.Entities;
+
+namespace Pandape.Host.Mvc.Data
+{
+    public class PandapeContext : DbContext
+    {
+        public PandapeContext (DbContextOptions<PandapeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pandape.Domain.Entities.Candidate> Candidate { get; set; }
+    }
+}
