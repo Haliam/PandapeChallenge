a4b1e8ab2beb917f6dbf3edd532221b7d0cfa32d
Haliam <haliampf@gmail.com>
2023-04-02 21:23:53 +0100

    Add ContextTest

---
+++ test/Pandape.Test/Insfrastructure/ContextTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Pandape.Infrastructure.Persistence.DataBase;
+using Xunit;
+
+namespace Pandape.Test.Insfrastructure
+{
+    public class ContextTest
+    {
+        [Theory]
+        [MemberData(nameof(ContextTestData.ConnectionString), MemberType = typeof(ContextTestData))]
+        public void ConnectionString_ShouldConnectToDatabase(string connectionString)
+        {
+            var options = new DbContextOptionsBuilder<PandapeContext>().UseSqlServer(connectionString).Options;
+            var context = new PandapeContext(options);
+            context.Database.CanConnect().Should().BeTrue();
+        }
+    }
+}
---
+++ test/Pandape.Test/Insfrastructure/ContextTestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pandape.Test.Insfrastructure
+{
+    public class ContextTestData
+    {
+       public static IEnumerable<object[]> ConnectionString =>
+       new List<object[]>
+       {
+            new object[]
+            {
+                "Server=(localdb)\\mssqllocaldb;Database=PandapeDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+            }
+       };
+    }
+}
