e58b77a8fae65584b5a53184bfcd3d4316e348f2
Haliam <haliampf@gmail.com>
2023-04-03 17:12:09 +0100

    Add basics commands

---
+++ src/Application/CQRS/Commands/DeleteCandidateCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Pandape.Application.CQRS.Responses;
+
+namespace Pandape.Application.CQRS.Commands
+{
+    public class DeleteCandidateCommand : IRequest<DeleteCandidateResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Application/CQRS/Commands/InsertCandidateCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Pandape.Application.CQRS.Responses;
+using Pandape.Domain.Entities;
+
+namespace Pandape.Application.CQRS.Commands
+{
+    public class InsertCandidateCommand : IRequest<InsertCandidateResponse>
+    {
+        public Candidate Candidate { get; set; }
+    }
+}
