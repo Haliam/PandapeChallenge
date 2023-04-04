5f8b3d19cd08c1190400498eb59552473b99f8af
Haliam <haliampf@gmail.com>
2023-04-04 20:21:50 +0100

    Add command query Candidate services implementation

---
+++ src/Application/AppServices/CommandCandidateAppService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Pandape.Application.CQRS.Commands;
+using Pandape.Application.CQRS.Responses;
+using Pandape.Domain.Entities;
+using Pandape.Infrastructure.Persistence.Repositories;
+using System.Threading.Tasks;
+
+namespace Pandape.Application.AppServices
+{
+    public class CommandCandidateAppService : AppServiceBase, ICommandCandidateAppService
+    {
+        protected ICandidateCommandRepository CandidateCommandRepository { get; }
+
+        public CommandCandidateAppService(ICandidateCommandRepository candidateCommandRepository, IMapper mapper) 
+            : base(mapper)
+        {
+            CandidateCommandRepository = candidateCommandRepository;
+        }
+
+        public async Task<InsertCandidateResponse> Insert(InsertCandidateCommand command)
+        {
+            var candidate = Mapper.Map<Candidate>(command);
+
+            await CandidateCommandRepository.Insert(candidate);
+
+            var insertCandidateResponse = new InsertCandidateResponse { IsInserted = true};
+
+            return insertCandidateResponse;
+        }
+
+        public async Task<UpdateCandidateResponse> Update(UpdateCandidateCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<DeleteCandidateResponse> Delete(DeleteCandidateCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/AppServices/QueryCandidateAppService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Pandape.Application.CQRS.Queries;
+using Pandape.Application.CQRS.Responses;
+using Pandape.Infrastructure.Persistence.Repositories;
+using System.Threading.Tasks;
+
+namespace Pandape.Application.AppServices
+{
+    public class QueryCandidateAppService : AppServiceBase, IQueryCandidateAppService
+    {
+        protected ICandidateQueryRepository CandidateQueryRepository { get; }
+
+        public QueryCandidateAppService(IMapper mapper, ICandidateQueryRepository candidateQueryRepository) 
+            : base(mapper)
+        {
+            CandidateQueryRepository = candidateQueryRepository;
+        }
+
+        public async Task<FindCandidateResponse> Find(FindCandidateQuery query)
+        {
+            var findCandidateResponse = new FindCandidateResponse();
+
+            //await CandidateQueryRepository.Find();
+
+            return findCandidateResponse;
+        }
+
+        public async Task<GetAllCandidatesResponse> GetAll()
+        {
+            var candidates = await CandidateQueryRepository.GetAll();
+
+            var getAllCandidatesResponse = new GetAllCandidatesResponse();
+
+            getAllCandidatesResponse.Candidates = candidates;
+
+            //var getAllCandidatesResponse = Mapper.Map<GetAllCandidatesResponse>(candidates);
+
+            return getAllCandidatesResponse;
+        }
+
+        public async Task<GetByIdCandidateResponse> GetById(GetByIdCandidateQuery query)
+        {
+            var id = Mapper.Map<int>(query.Id);
+
+            var candidate = await CandidateQueryRepository.GetById(id);
+
+            var getByIdCandidateResponse = Mapper.Map<GetByIdCandidateResponse>(candidate);
+
+            return getByIdCandidateResponse;
+        }
+    }
+}
