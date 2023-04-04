d8cd002b6abe791c876647b88f5ae8edd913e1c0
Haliam <haliampf@gmail.com>
2023-04-04 20:15:23 +0100

    Add AppServiceBase, query and command interface

---
+++ src/Application/AppServices/AppServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Pandape.Application.AppServices
+{
+    public abstract class AppServiceBase
+    {
+        protected IMapper Mapper { get; }
+
+        public AppServiceBase(IMapper mapper )
+        {
+            Mapper = mapper;
+        }
+    }
+}
---
+++ src/Application/AppServices/IQueryCandidateAppService.cs
@@ -0,0 +1,15 @@
+﻿using Pandape.Application.CQRS.Queries;
+using Pandape.Application.CQRS.Responses;
+using System.Threading.Tasks;
+
+namespace Pandape.Application.AppServices
+{
+    public interface IQueryCandidateAppService
+    {
+        Task<FindCandidateResponse> Find(FindCandidateQuery query);
+
+        Task<GetAllCandidatesResponse> GetAll();
+
+        Task<GetByIdCandidateResponse> GetById(GetByIdCandidateQuery query);
+    }
+}
