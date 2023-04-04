8c16f2cf32384abb35d09de85eea0a4c0d885b33
Haliam <haliampf@gmail.com>
2023-04-04 19:59:37 +0100

    Add Base Handlers

---
+++ src/Application/CQRS/Handlers/QueryHandlerBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Pandape.Application.AppServices;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Pandape.Application.CQRS.Handlers
+{
+    public abstract class QueryHandlerBase<TRequest, TResponse> 
+        : IRequestHandler<TRequest, TResponse> where TRequest 
+        : IRequest<TResponse>
+    {
+        protected IQueryCandidateAppService QueryCandidateAppService { get; }
+
+        public QueryHandlerBase( IQueryCandidateAppService queryCandidateAppService )
+        {
+            QueryCandidateAppService = queryCandidateAppService;
+        }
+
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    }
+}
