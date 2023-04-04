737818125cb41e466099342980049f9c32965773
Haliam <haliampf@gmail.com>
2023-04-04 20:04:08 +0100

    Add Handlers

---
+++ src/Application/CQRS/Handlers/GetAllCandidatesQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Pandape.Application.AppServices;
+using Pandape.Application.CQRS.Queries;
+using Pandape.Application.CQRS.Responses;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pandape.Application.CQRS.Handlers
+{
+    public class GetAllCandidatesQueryHandler : QueryHandlerBase<GetAllCandidatesQuery, GetAllCandidatesResponse>
+    {
+        public GetAllCandidatesQueryHandler(IQueryCandidateAppService queryCandidateAppService) 
+            : base(queryCandidateAppService)
+        {
+        }
+
+        public override Task<GetAllCandidatesResponse> Handle(GetAllCandidatesQuery query, CancellationToken cancellationToken) =>
+            QueryCandidateAppService.GetAll();
+    }
+}
