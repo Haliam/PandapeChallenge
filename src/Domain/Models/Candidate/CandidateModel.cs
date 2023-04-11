ccff2e4e75a8147ed4b6d9d166229fe0651e7bce
Haliam <haliampf@gmail.com>
2023-04-11 17:53:41 +0100

    Add CandidateModel

---
+++ src/Domain/Models/Candidate/CandidateModel.cs
@@ -0,0 +1,43 @@
+﻿using Pandape.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pandape.Domain.Models.Candidate
+{
+    public class CandidateModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string SurName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Column(TypeName = "varchar(250)")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime InsertDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime? ModifyDate { get; set; }
+
+        public ICollection<CandidateExperience> CandidateExperiences { get; set; }
+    }
+}
