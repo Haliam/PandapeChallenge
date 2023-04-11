31083fe64270addb276703d938dd1903901f544a
Haliam <haliampf@gmail.com>
2023-04-11 17:55:47 +0100

    Add mapping

---
+++ src/Host/Pandape.Host.Mvc/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace Pandape.Host.Mvc.Mapping
+{
+    public class AutoMapperConfig
+    {
+
+        public AutoMapperConfig()
+        {
+            InitializeAutomapper();
+        }
+
+        private void InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new CandidateProfile());
+            });
+
+            IMapper mapper = config.CreateMapper();
+        }
+    }
+}
---
+++ src/Host/Pandape.Host.Mvc/Mapping/CandidateProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Pandape.Domain.Models.Candidate;
+using Pandape.Host.Mvc.ViewModels;
+
+namespace Pandape.Host.Mvc.Mapping
+{
+    public class CandidateProfile : Profile
+    {
+        public CandidateProfile() 
+        { 
+            CreateMap<CandidateModel, CandidateViewModel>().ReverseMap();
+        }
+    }
+}
