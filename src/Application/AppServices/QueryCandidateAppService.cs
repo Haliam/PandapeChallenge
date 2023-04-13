2d197a548437f555aa55240c193969b25286ec22
Haliam <haliampf@gmail.com>
2023-04-13 16:34:05 +0100

    Add dtos to response and serilog to QueryCandidateAppService

--- src/Application/AppServices/QueryCandidateAppService.cs
+++ src/Application/AppServices/QueryCandidateAppService.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Pandape.Application.CQRS.Queries;
 using Pandape.Application.CQRS.Responses;
+using Pandape.Application.Dtos;
 using Pandape.Domain.Entities;
 using Pandape.Infrastructure.Persistence.Repositories;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,46 +16,121 @@ namespace Pandape.Application.AppServices
     {
         protected ICandidateQueryRepository CandidateQueryRepository { get; }
 
-        public QueryCandidateAppService(IMapper mapper, ICandidateQueryRepository candidateQueryRepository) 
+        protected ILogger<QueryCandidateAppService> Log { get; }
+
+        public QueryCandidateAppService(IMapper mapper, ILogger<QueryCandidateAppService> logger, ICandidateQueryRepository candidateQueryRepository) 
             : base(mapper)
         {
             CandidateQueryRepository = candidateQueryRepository;
+
+            Log = logger;
         }
 
         public async Task<FindCandidateResponse> Find(FindCandidateQuery query)
         {
-            var candidates = await CandidateQueryRepository.Find(query.Expression);
+            var response = new FindCandidateResponse();
 
-            var response = new FindCandidateResponse { Candidates = candidates };
+            try
+            {
+                var candidates = await CandidateQueryRepository.Find(query.Expression);
+
+                response.CandidatesDto = Mapper.Map<IEnumerable<CandidateDto>>(candidates);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"{nameof(QueryCandidateAppService)} - {nameof(FindCandidateResponse)} Error: {ex.Message}");
+            }
 
             return response;
         }
 
         public async Task<GetAllCandidatesResponse> GetAll()
         {
-            var candidates = await CandidateQueryRepository.GetAll();
+            var response = new GetAllCandidatesResponse();
 
-            var response = new GetAllCandidatesResponse { Candidates = Mapper.Map<IEnumerable<Candidate>>(candidates) };
+            try
+            {
+                var candidates = await CandidateQueryRepository.GetAll();
+
+                response.CandidatesDto = Mapper.Map<IEnumerable<CandidateDto>>(candidates);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"{nameof(QueryCandidateAppService)} - {nameof(GetAllCandidatesResponse)} Error: {ex.Message}");
+            }
 
             return response;
         }
 
         public async Task<GetByIdCandidateResponse> GetById(GetByIdCandidateQuery query)
         {
-            var candidate = await CandidateQueryRepository.GetById(query.Id);
+            var response = new GetByIdCandidateResponse();
 
-            var response = new GetByIdCandidateResponse { Candidate = candidate };
+            try
+            {
+                var candidate = await CandidateQueryRepository.GetById(query.Id);
+
+                response.CandidateDto = Mapper.Map<CandidateDto>(candidate);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"{nameof(QueryCandidateAppService)} - {nameof(GetByIdCandidateResponse)} Error: {ex.Message}");
+            }
 
             return response;
         }
 
         public async Task<GetDetailsCandidateResponse> GetDetails(GetDetailsCandidateQuery query)
         {
-            var candidate = await CandidateQueryRepository.GetById(query.Id);
+            var response = new GetDetailsCandidateResponse();
 
-            var response = new GetDetailsCandidateResponse { Candidate = candidate };
+            var candidateExperiences = GetCandidateExperiences();
+
+            try
+            {
+                var candidate = await CandidateQueryRepository.GetById(query.Id);
+
+                response.CandidateDto = Mapper.Map<CandidateDto>(candidate);
+
+                response.CandidateExperiencesDto = Mapper.Map<List<CandidateExperienceDto>>(candidateExperiences);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"{nameof(QueryCandidateAppService)} - {nameof(GetDetailsCandidateResponse)} Error: {ex.Message}");
+            }
 
             return response;
+        }
+
+
+        private List<CandidateExperience> GetCandidateExperiences()
+        {
+            var candidateExperiences = new List<CandidateExperience>
+            {
+
+                new CandidateExperience
+                {
+                    Company = "Google",
+                    Job = "Developer",
+                    Description= "Description",
+                    Salary = 40000,
+                    BeginDate= DateTime.Now.Date,
+                    EndDate = null,
+                    InsertDate = DateTime.Now.Date,
+                },
+                new CandidateExperience
+                {
+                    Company = "Imexin",
+                    Job = "Chemistry",
+                    Description= "Description",
+                    Salary = 50000,
+                    BeginDate= DateTime.Now.Date,
+                    EndDate = null,
+                    InsertDate = DateTime.Now.Date,
+                },
+            };
+
+            return candidateExperiences;
         }
     }
 }
